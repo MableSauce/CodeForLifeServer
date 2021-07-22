f638f51d784ded3386d3fd1da89b71ab7b46fbe8
Abel Perez <abel.perez1342@gmail.com>
2021-07-22 17:54:40 -0400

    first commit

---
+++ superhero.js
@@ -0,0 +1,14 @@
+class Superheros{
+
+    constructor(heroPowerLevel, heroName){
+    
+    this.heroPowerLevel = heroPowerLevel;
+    this.heroName = heroName;
+    };
+    
+    }
+    console.log(Superheros);
+    
+    const bacon = new Superheros([31,40,20,10,200], ["bob", "dog","cat", "pop", "ur mom"]);
+    
+    console.log(bacon);
